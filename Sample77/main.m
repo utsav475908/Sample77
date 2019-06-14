a2de8669dc3e4df8e001af652c56f09f839cb63c
Kumar here <utsav.kumar@honeywell.com>
2019-06-14 23:06:09 +0530

    done with love

---
+++ Sample77/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Sample77
+//
+//  Created by Workout on 14/06/19.
+//  Copyright © 2019 Workout. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
